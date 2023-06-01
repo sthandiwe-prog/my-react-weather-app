b27627070a6767ac1c809957e9d14905338854cb
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2023-06-01 23:45:03 +0200

    Added API to the project

--- src/App.js
+++ src/App.js
@@ -1,62 +1,91 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <form className="mb-4 mt-2">
-          <div className="row">
-            <div className="col-9">
-              {" "}
-              <input
-                type="search"
-                className="form-control"
-                placeholder="Enter a city..."
-              />{" "}
-            </div>
-            <div className="col-3">
-              {" "}
-              <input
-                type="submit"
-                value="Search"
-                className=" btn btn-danger w-100"
-              />
-            </div>
-          </div>
-        </form>
+  let city = "Paris";
+  const [weather, setWeatherData] = useState({ ready: false });
 
-        <h1>Weather App</h1>
-        <div className="row">
-          <div className="col-6">
-            {" "}
-            <ul>
-              <li> Thursday 13:43 </li>
-              <li> fog </li>
-              <li>
-                <div className="clearfix">
-                  {" "}
-                  <img
-                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                    className="float-start"
-                    alt="fog"
-                  />{" "}
-                  <div className="float-start">
-                    <span className="temperature ms-2"> 27 </span>
-                    <span className="celsius-temp">°C</span>
+  function updateData(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      name: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      date: `Thursday 11:00`,
+      icon: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+    });
+  }
+
+  if (weather.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <form className="mb-4 mt-2">
+            <div className="row">
+              <div className="col-9">
+                {" "}
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Enter a city..."
+                />{" "}
+              </div>
+              <div className="col-3">
+                {" "}
+                <input
+                  type="submit"
+                  value="Search"
+                  className=" btn btn-danger w-100"
+                />
+              </div>
+            </div>
+          </form>
+
+          <h1>{weather.name}</h1>
+          <div className="row">
+            <div className="col-6">
+              {" "}
+              <ul>
+                <li>{weather.date} </li>
+                <li className="text-capitalize"> {weather.description} </li>
+                <li>
+                  <div className="clearfix">
+                    {" "}
+                    <img
+                      src={weather.icon}
+                      className="float-start"
+                      alt={weather.description}
+                    />{" "}
+                    <div className="float-start">
+                      <span className="temperature ms-2">
+                        {" "}
+                        {weather.temperature}{" "}
+                      </span>
+                      <span className="celsius-temp">°C</span>
+                    </div>
                   </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity : 78% </li>
-              <li>Wind : 1.54 km/h</li>
-            </ul>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity : {weather.humidity}% </li>
+                <li>Wind : {weather.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `b98755d1364b40ce6f0dab6b8d71729b`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateData);
+  }
 }
