2c2d65d268e8877ec8539c3b9165b867c13c8dfd
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2023-06-01 15:02:04 +0200

    Added html skeleton for my weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Welcome on React Sthandiwe!</h1>
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              {" "}
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Enter a city..."
+              />{" "}
+            </div>
+            <div className="col-3">
+              {" "}
+              <input type="submit" value="Search" className=" btn btn-danger" />
+            </div>
+          </div>
+        </form>
+
+        <h1>Weather App</h1>
+        <div className="row">
+          <div className="col-6">
+            {" "}
+            <ul>
+              <li> Thursday 13:43 </li>
+              <li> fog </li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="fog"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity : 78% </li>
+              <li>Wind : 1.54 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
